851497e707b128b2037dbc4cf0168e7a7262bd79
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2022-11-04 11:40:17 +0800

    Sorting Kategori

--- app/Controllers/Api/Kategori.php
+++ app/Controllers/Api/Kategori.php
@@ -1,6 +1,9 @@
-<?php namespace App\Controllers\Api;
+<?php
+
+namespace App\Controllers\Api;
 
 use App\Controllers\MyResourceController;
+
 /**
  * Class Kategori
  * @note Resource untuk mengelola data m_kategori
@@ -13,17 +16,29 @@ class Kategori extends MyResourceController
     protected $format    = 'json';
 
     protected $rulesCreate = [
-       'title' => ['label' => 'title', 'rules' => 'required'],
-       'show' => ['label' => 'show', 'rules' => 'required'],
-       'urutan' => ['label' => 'urutan', 'rules' => 'required'],
-       'deletedAt' => ['label' => 'deletedAt', 'rules' => 'required'],
-   ];
+        'title' => ['label' => 'title', 'rules' => 'required'],
+        'show' => ['label' => 'show', 'rules' => 'required'],
+        'urutan' => ['label' => 'urutan', 'rules' => 'required'],
+        'deletedAt' => ['label' => 'deletedAt', 'rules' => 'required'],
+    ];
 
     protected $rulesUpdate = [
-       'title' => ['label' => 'title', 'rules' => 'required'],
-       'show' => ['label' => 'show', 'rules' => 'required'],
-       'urutan' => ['label' => 'urutan', 'rules' => 'required'],
-       'deletedAt' => ['label' => 'deletedAt', 'rules' => 'required'],
-   ];
+        'title' => ['label' => 'title', 'rules' => 'required'],
+        'show' => ['label' => 'show', 'rules' => 'required'],
+        'urutan' => ['label' => 'urutan', 'rules' => 'required'],
+        'deletedAt' => ['label' => 'deletedAt', 'rules' => 'required'],
+    ];
 
+    public function index()
+    {
+        $data = parent::index();
+        $data = json_decode($data->getBody(), true);
+
+        foreach ($data['data']['rows'] as $kK => $vK) {
+            sort($vK['menu']);
+            $data['data']['rows'][$kK]['menu'] = $vK['menu'];
+        }
+        
+        return $this->respond($data);
+    }
 }
